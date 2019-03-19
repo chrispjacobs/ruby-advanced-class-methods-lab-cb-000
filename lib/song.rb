c90ea7036b385bd9db3642ae9f9338a10e16919d
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-19 17:16:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -70,7 +70,8 @@ song_file_name = "The Beatles - Help!.mp3"
 split_array = []
 split_array << song_file_name.split(" - ")
 generated = Song.new
-split_array[1] = split_array[1].split(".")[0]
+good_version_of_title = []
+good_version_of_title << split_array[1].split(".")
 puts split_array[1]
 #generated.artist_name=(split_array[0])
 #generated.name=(split_array[1])
